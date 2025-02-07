81db3da1bbdc95105b3260b9e9ac92f5bf0e3a64
YangYangJiJi <jihyunnamu@gmail.com>
2025-02-07 17:51:35 +0900

    move misplaced folders back into Stronghold

--- 002_RNA/002_RNA.py
+++
@@ -1,6 +0,0 @@
-with open('data/rosalind_rna.txt') as input_data:
-    dna = input_data.read().strip()
-
-with open('output/002_RNA.txt', 'w') as output_data:
-    output_data.write(dna.replace('T', 'U'))
-    print (dna.replace('T', 'U'))
